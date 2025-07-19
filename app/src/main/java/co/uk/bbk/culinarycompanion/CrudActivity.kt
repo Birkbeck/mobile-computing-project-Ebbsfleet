c54e50cbd46c061aefecd9fb16254ada2162cfa5
Ebbsfleet <mike@contactbrown.com>
2025-07-19 23:25:59 +0100

    Basic buttons (again)

--- app/src/main/java/co/uk/bbk/culinarycompanion/CrudActivity.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion/CrudActivity.kt
@@ -1,11 +1,25 @@
 package co.uk.bbk.culinarycompanion
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class CrudActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crud)
+        val goToMainButton: Button = findViewById(R.id.button2)
+        goToMainButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+        }
     }
 }

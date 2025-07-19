fc0aa506325f78153b2a1b29e07ff2b640d4bba7
Ebbsfleet <mike@contactbrown.com>
2025-07-19 22:54:09 +0100

    Basic buttons

---
+++ app/src/main/java/co/uk/bbk/culinarycompanion/CrudActivity.kt
@@ -0,0 +1,11 @@
+package co.uk.bbk.culinarycompanion
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CrudActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crud)
+    }
+}
--- app/src/main/java/co/uk/bbk/culinarycompanion/MainActivity.kt
+++ app/src/main/java/co/uk/bbk/culinarycompanion/MainActivity.kt
@@ -1,6 +1,8 @@
 package co.uk.bbk.culinarycompanion
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val goToCRUDButton: Button = findViewById(R.id.button)
+        goToCRUDButton.setOnClickListener {
+            val intent = Intent(this, CrudActivity::class.java)
+            startActivity(intent)
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
     }
 }

0c7d36fb901298a4cec902b7a95f1ac0a17ac467
Ebbsfleet <mike@contactbrown.com>
2025-07-20 02:09:40 +0100

    Define recipe entity.

---
+++ app/src/main/java/co/uk/bbk/culinarycompanion/Recipes.kt
@@ -0,0 +1,14 @@
+package co.uk.bbk.culinarycompanion
+import androidx.room.Room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recipes")
+data class recipe(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val ingredients: String,
+    val instructions: String,
+    val category: String
+)
